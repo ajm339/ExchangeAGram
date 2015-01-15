ddca084410e02e2f0a4d916c2bd53ce3eb5bd02b
Alex Meyers <ajm339@cornell.edu>
2015-01-14 22:07:13 -0500

    Through ExchangeAGram: Displaying the FeedItem with error referring to Cell in dequeue call

---
+++ ExchangeAGram/FeedItem.swift
@@ -0,0 +1,18 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by Alex Meyers on 1/14/15.
+//  Copyright (c) 2015 alex. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc (FeedItem)
+class FeedItem: NSManagedObject {
+
+    @NSManaged var caption: String
+    @NSManaged var image: NSData
+
+}
---
+++ FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Alex Meyers on 1/14/15.
+//  Copyright (c) 2015 alex. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
