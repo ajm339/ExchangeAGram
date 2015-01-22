e22ac26ec371a0b5e8797c5e515c0b762ccd7c4e
Alex Meyers <ajm339@cornell.edu>
2015-01-21 20:44:45 -0500

    Profile Segue and Testing (with FB login) complete

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Alex Meyers on 1/21/15.
+//  Copyright (c) 2015 alex. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
